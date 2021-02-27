000473b575fbe0b154da2495034ab9366104b30b
nitensolanki02@gmail.com <nitensolanki02@gmail.com>
2021-02-27 18:19:10 +0530

    some fixes

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,14 +1,12 @@
-import React, { useEffect, useState } from "react";
-import MovieCard from "./MovieCard";
+import React, { useEffect } from "react";
 import { fetchMovies } from "../thunk_functions/fetchMovies";
 import { useDispatch } from "react-redux";
 
 const Child = () => {
-	const [page, setPage] = useState(1);
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-		dispatch(fetchMovies(page));
+		dispatch(fetchMovies(1));
 	}, []);
 
 	return <div></div>;
